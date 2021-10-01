e8d3d50b4b343300afa63c67aac22de015c2d6ff
YuZhangWang <2277598524@qq.com>
2021-10-01 22:21:37 +0800

    update

---
+++ 01HelloClion/cmake-build-debug/CMakeFiles/01HelloClion.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "01HelloClion.exe"
+  "01HelloClion.exe.manifest"
+  "01HelloClion.pdb"
+  "CMakeFiles/01HelloClion.dir/main.c.obj"
+  "lib01HelloClion.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/01HelloClion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
